4bd147657f6ec19a99b8ff7774cc2767958deba3
Filipe Silva <silvam.filipe@gmail.com>
2021-01-28 12:12:20 -0100

    Adds query and pagination support interfaces

---
+++ src/Application/Query.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of forum
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Application;
+
+/**
+ * Query
+ *
+ * @package App\Application
+ */
+interface Query
+{
+
+    /**
+     * Returns a query result for provided attribute list
+     *
+     * @param array $attributes
+     *
+     * @return QueryResult
+     */
+    public function data(array $attributes = []): QueryResult;
+
+}
