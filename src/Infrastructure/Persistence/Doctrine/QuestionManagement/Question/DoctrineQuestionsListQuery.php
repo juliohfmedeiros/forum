3e1aea22d463b66b6e3373f353d9c0d40317221b
Filipe Silva <silvam.filipe@gmail.com>
2021-02-07 23:29:59 -0100

    Adds my questions list

--- src/Infrastructure/Persistence/Doctrine/QuestionManagement/Question/DoctrineQuestionsListQuery.php
+++ src/Infrastructure/Persistence/Doctrine/QuestionManagement/Question/DoctrineQuestionsListQuery.php
@@ -13,6 +13,7 @@ use App\Application\Pagination;
 use App\Application\QueryResult;
 use App\Application\QuestionManagement\QuestionsListQuery;
 use Doctrine\DBAL\Driver\Connection;
+use Doctrine\DBAL\Exception as DBALException;
 use Doctrine\DBAL\Query\QueryBuilder;
 
 /**
@@ -47,37 +48,99 @@ final class DoctrineQuestionsListQuery implements QuestionsListQuery
      */
     public function data(array $attributes = []): QueryResult
     {
-
-
         $queryBuilder = $this->connection->createQueryBuilder()
             ->from("questions", "q")
             ->leftJoin("q", "users", "u", "u.id = q.owner_id");
 
-        $pagination = new Pagination($this->getTotalRows($queryBuilder), 10);
+        $this->checkOwnerClause($attributes, $queryBuilder);
+        $this->checkPatternFilter($attributes, $queryBuilder);
 
+        $pagination = (
+            new Pagination($this->getTotalRows($queryBuilder), $attributes['rows'])
+        )
+            ->forPage($attributes["page"]);
 
         $queryBuilder
-            ->select("q.id as questionId, q.title, q.body, q.open, u.name, q.applied_on as appliedOn")
+            ->select("q.id as questionId, q.title, q.body, q.open, u.id as ownerId, q.applied_on as appliedOn, u.name as ownerName, u.email as ownerEmail")
             ->setFirstResult($pagination->firstResult())
             ->setMaxResults($pagination->maxResults());
 
 
-        $stm = $this->connection->executeQuery($queryBuilder->getSQL());
+        $stm = $queryBuilder->execute();
 
         $result = new QueryResult($stm->fetchAllAssociative());
+        $this->updateAttributes(['pattern', 'page', 'rows'], $attributes, $result);
         $result->withPagination($pagination);
         return $result;
     }
 
+    /**
+     * Executes the filtered query to retrieve the total rows
+     *
+     * @param QueryBuilder $queryBuilder
+     * @return int
+     * @throws DBALException
+     */
     private function getTotalRows(QueryBuilder $queryBuilder)
     {
         $qb = clone $queryBuilder;
         $qb->select("count(*) as total");
-        $result = $this->connection
-            ->executeQuery(
-                $qb->getSQL()
-            )
-            ->fetchOne();
+        $result = $qb->execute()->fetchOne();
         return (int) $result;
+    }
+
+    /**
+     * Adds the owner ID in query filter
+     *
+     * @param array $attributes
+     * @param QueryBuilder $queryBuilder
+     */
+    private function checkOwnerClause(array $attributes, QueryBuilder $queryBuilder)
+    {
+        if (!array_key_exists("ownerId", $attributes)) {
+            return;
+        }
+
+        $queryBuilder->where("q.owner_id = :ownerId");
+        $queryBuilder->setParameter('ownerId', $attributes['ownerId']);
+    }
+
+    /**
+     * Adds search pattern filter
+     *
+     * @param array        $attributes
+     * @param QueryBuilder $builder
+     */
+    private function checkPatternFilter(array $attributes, QueryBuilder $builder)
+    {
+        if (!array_key_exists('pattern', $attributes) || $attributes['pattern'] == null) {
+            return;
+        }
+
+        $builder
+            //->where('q.tags LIKE :pattern')
+            ->where('q.title LIKE :pattern')
+            ->orWhere('u.email LIKE :pattern')
+            ->orWhere('u.name LIKE :pattern')
+            ->orWhere('q.body LIKE :pattern')
+
+            ->setParameter('pattern', "%{$attributes['pattern']}%")
+        ;
+    }
+
+    /**
+     * Updates the query result attributes
+     *
+     * @param array $array
+     * @param array $attributes
+     * @param QueryResult $result
+     */
+    private function updateAttributes(array $array, array $attributes, QueryResult $result)
+    {
+        foreach ($array as $attribute) {
+            if (array_key_exists($attribute, $attributes)) {
+                $result->addAttribute($attribute, $attributes[$attribute]);
+            }
+        }
     }
 }
--- src/UserInterface/QuestionManagement/MyQuestionsList.php
+++ src/UserInterface/QuestionManagement/MyQuestionsList.php
@@ -11,7 +11,10 @@ namespace App\UserInterface\QuestionManagement;
 
 use App\Application\QuestionManagement\QuestionsListQuery;
 use App\UserInterface\ApiControllerMethods;
+use App\UserInterface\UserManagement\OAuth2\AuthenticatedControllerInterface;
+use App\UserInterface\UserManagement\OAuth2\AuthenticatedControllerMethods;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,10 +23,12 @@ use Symfony\Component\Routing\Annotation\Route;
  *
  * @package App\UserInterface\QuestionManagement
  */
-final class MyQuestionsList extends AbstractController
+final class MyQuestionsList extends AbstractController implements AuthenticatedControllerInterface
 {
 
     use ApiControllerMethods;
+
+    use AuthenticatedControllerMethods;
 
     /**
      * @var QuestionsListQuery
@@ -46,12 +51,88 @@ final class MyQuestionsList extends AbstractController
      * @return Response
      * @Route(path="/questions", methods={"GET"})
      */
-    public function handle(): Response
+    public function handle(Request $request): Response
     {
         return new Response(
-            json_encode($this->questionsList->data()),
+            json_encode($this->questionsList->data([
+                "ownerId" => $this->currentUser()->userId(),
+                "page" => $request->query->get('page', 1),
+                "rows" => $request->query->get('rows', 10),
+                "pattern" => $request->query->get('pattern', null)
+            ])),
             200,
             ["content-type" => "application/json"]
         );
     }
 }
+
+/**
+ * @OA\Get(
+ *     path="/questions",
+ *     tags={"Questions"},
+ *     summary="Lists the questions of current logged in user",
+ *     operationId="readMyQuestions",
+ *     @OA\Parameter(
+ *         name="page",
+ *         in="query",
+ *         description="Start page",
+ *         required=false,
+ *         @OA\Schema(type="integer")
+ *     ),
+ *     @OA\Parameter(
+ *         name="rows",
+ *         in="query",
+ *         description="Number of rows per page",
+ *         required=false,
+ *         @OA\Schema(type="integer")
+ *     ),
+ *     @OA\Parameter(
+ *         name="pattern",
+ *         in="query",
+ *         description="Filters result with a search pattern",
+ *         required=false,
+ *         @OA\Schema(type="string")
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="A list of user questions",
+ *         @OA\JsonContent(ref="#/components/schemas/QuestionList")
+ *     ),
+ *     security={
+ *         {"OAuth2.0-Token": {"user.management"}}
+ *     }
+ * )
+ */
+
+/**
+ * @OA\Schema(
+ *     schema="ListingQuestion",
+ *     title="Listing Question",
+ *     type="object",
+ *     @OA\Property(property="questionId", type="string", example="e1026e90-9b21-4b6d-b06e-9c592f7bdb82"),
+ *     @OA\Property(property="title", type="string", example="What time is it in Boston?"),
+ *     @OA\Property(property="body", type="string", example="A longer consideration on how to ask for current time in Boston."),
+ *     @OA\Property(property="open", type="boolean", example=false),
+ *     @OA\Property(property="ownerId", type="string", example="e1026e90-9b21-4b6d-b06e-9c592f7bdb82"),
+ *     @OA\Property(property="appliedOn", type="string", example="021-01-27 15:59:01"),
+ *     @OA\Property(property="ownerName", type="string", example="John Doe"),
+ *     @OA\Property(property="ownerEmail", type="string", example="john.doe@example.com"),
+ * )
+ */
+
+/**
+ * @OA\Schema(
+ *     schema="QuestionList",
+ *     type="object",
+ *     @OA\Property(
+ *          property="attributes",
+ *          type="object",
+ *          @OA\AdditionalProperties(
+ *              type="string"
+ *          )
+ *     ),
+ *     @OA\Property(property="data", type="array", @OA\Items(ref="#/components/schemas/ListingQuestion")),
+ *     @OA\Property(property="count", type="integer", example=32),
+ *     @OA\Property(property="isEmpty", type="bool", example=false),
+ * )
+ */
